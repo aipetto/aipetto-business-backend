3ae0ffa7bd2203b3cfaa8a34f5816bcb7fcde93d
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-05-25 17:57:19 -0300

    Accept landing page surveys without permission checker on user email validation and for AIPETTO Management workspace

--- src/api/landingSurvey/landingSurveyCreate.ts
+++ src/api/landingSurvey/landingSurveyCreate.ts
@@ -1,13 +1,8 @@
-import PermissionChecker from '../../services/user/permissionChecker';
 import ApiResponseHandler from '../apiResponseHandler';
-import Permissions from '../../security/permissions';
 import LandingSurveyService from '../../services/landingSurveyService';
 
 export default async (req, res, next) => {
   try {
-    new PermissionChecker(req).validateHas(
-      Permissions.values.landingSurveyCreate,
-    );
 
     const payload = await new LandingSurveyService(req).create(
       req.body.data,
