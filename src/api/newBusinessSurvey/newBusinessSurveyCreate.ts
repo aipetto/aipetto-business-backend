f0553471030a98f5472ba4ca116ba198d794cd60
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-06-10 00:03:33 -0300

    Remove Permission checker from Business Landing Page

--- src/api/newBusinessSurvey/newBusinessSurveyCreate.ts
+++ src/api/newBusinessSurvey/newBusinessSurveyCreate.ts
@@ -1,15 +1,9 @@
-import PermissionChecker from '../../services/user/permissionChecker';
 import ApiResponseHandler from '../apiResponseHandler';
-import Permissions from '../../security/permissions';
 import NewBusinessSurveyService from '../../services/newBusinessSurveyService';
 
 export default async (req, res, next) => {
   try {
-    new PermissionChecker(req).validateHas(
-      Permissions.values.newBusinessSurveyCreate,
-    );
-
-    const payload = await new NewBusinessSurveyService(req).create(
+      const payload = await new NewBusinessSurveyService(req).create(
       req.body.data,
     );
 
