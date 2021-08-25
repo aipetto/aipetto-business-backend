566f16639d151103b518e6a7a5300bf897e67bd8
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-08-25 14:33:11 -0300

    Boost new developing version to v1.0.5-alpha

--- src/api/place/index.ts
+++ src/api/place/index.ts
@@ -24,7 +24,7 @@ export default (app) => {
     require('./placeList').default,
   );
   app.get(
-      `/place/geolocation`,
+      `/place/nearby`,
       require('./findPlacesNearbyByGeolocation').default,
   );
   app.get(
