c63774416d9564f265b928291777b1835f175dbb
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-10-25 23:40:15 -0300

    Allow creation of reservations without check user permissions

--- src/api/serviceReservation/serviceReservationCreate.ts
+++ src/api/serviceReservation/serviceReservationCreate.ts
@@ -1,14 +1,8 @@
-import PermissionChecker from '../../services/user/permissionChecker';
 import ApiResponseHandler from '../apiResponseHandler';
-import Permissions from '../../security/permissions';
 import ServiceReservationService from '../../services/serviceReservationService';
 
 export default async (req, res, next) => {
   try {
-    new PermissionChecker(req).validateHas(
-      Permissions.values.serviceReservationCreate,
-    );
-
     const payload = await new ServiceReservationService(req).create(
       req.body.data,
     );
