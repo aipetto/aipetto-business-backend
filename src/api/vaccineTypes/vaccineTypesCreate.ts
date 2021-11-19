a5f1adeb1108db31359b6c0c2c0314dbc7761fd2
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-11-19 12:03:56 -0300

    Remove permission checker on vaccine types registration

--- src/api/vaccineTypes/vaccineTypesCreate.ts
+++ src/api/vaccineTypes/vaccineTypesCreate.ts
@@ -1,14 +1,8 @@
-import PermissionChecker from '../../services/user/permissionChecker';
 import ApiResponseHandler from '../apiResponseHandler';
-import Permissions from '../../security/permissions';
 import VaccineTypesService from '../../services/vaccineTypesService';
 
 export default async (req, res, next) => {
   try {
-    new PermissionChecker(req).validateHas(
-      Permissions.values.vaccineTypesCreate,
-    );
-
     const payload = await new VaccineTypesService(req).create(
       req.body.data,
     );
